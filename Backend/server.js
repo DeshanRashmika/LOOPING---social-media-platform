c469dfb03e037e9f8c9a5a9a4b9d00ea6a8a5bff
Deshan Rashmika <deshanrashmika002@gmail.com>
2026-05-21 19:35:54 +0530

    initial commit

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ MongoDB Database Connected Successfully'))
+    .catch((err) => console.log('❌ Database Connection Error: ', err));
+
+app.get('/', (req, res) => {
+    res.send('Social Media API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
